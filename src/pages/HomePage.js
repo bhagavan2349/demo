31fbfb77e102a44bac654e603e0750dd1868144f
bhagavan2349 <136468519+bhagavan2349@users.noreply.github.com>
2023-06-13 19:08:23 +0530

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardListing from '../components/CardListing/CardListing';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <CardListing />
+    </div>
+  );
+};
+
+export default HomePage;
